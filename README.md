c76702390dd4220e820d92828fe7df95f8954b65
Pedro Morais <autranemorais@gmail.com>
2018-12-09 13:47:31 -0800

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # cassie-rl
 An OpenAI Gym style reinforcement learning interface for Agility Robotics' biped robot Cassie 
+
+# Installation
+
+Run ```$ pip install -e .``` in root folder (the one that contains setup.py).
+
+# TODO:
+
+Add the OpenAI gym environment registration code so that the environment can be created with gym.make().
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+#REQUIRED_PACKAGES = ["torch==0.4", "gym", "numpy", "visdom"]
+
+setup(name='cassierl',
+      version='0.1',
+      description='Cassie reinforcement learning environment',
+      author='Pedro Morais',
+      author_email='autranemorais@gmail.com',
+      license='MIT',
+      packages=['cassie'],
+      #install_requires=REQUIRED_PACKAGES
+)
